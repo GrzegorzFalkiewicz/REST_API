e5678d3d4e8d9b1fbc070e43dd85f2007da43b9c
Grzegorz Falkiewicz (Jelon) <95246683+GrzegorzFalkiewicz@users.noreply.github.com>
2023-01-15 14:22:41 +0100

    Controller configuration
    adding the crud functions

--- app/Http/Controllers/PeopleController.php
+++ app/Http/Controllers/PeopleController.php
@@ -5,82 +5,36 @@ namespace App\Http\Controllers;
 use App\Models\People;
 use App\Http\Requests\StorePeopleRequest;
 use App\Http\Requests\UpdatePeopleRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class PeopleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+  public function index(): JsonResponse
     {
-        //
+        return response()->json(People::all(),200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function create(StorePeopleRequest $request): JsonResponse
     {
-        //
+        $people = People::create($request->all());
+        return response()->json($people, 201);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StorePeopleRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StorePeopleRequest $request)
+    public function read(People $people): JsonResponse
     {
-        //
+        return response()->json($people, 200);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\People  $people
-     * @return \Illuminate\Http\Response
-     */
-    public function show(People $people)
+    public function update(Request $request, People $people): JsonResponse
     {
-        //
+        $people->update($request->all());
+        return response()->json($people, 200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\People  $people
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(People $people)
+    public function delete(People $people): JsonResponse
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdatePeopleRequest  $request
-     * @param  \App\Models\People  $people
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdatePeopleRequest $request, People $people)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\People  $people
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(People $people)
-    {
-        //
+        $people->delete();
+        return response()->json(null, 204);
     }
 }
